3194b5a21d0052c573ef91d5d3c0f9a4189da19a
Gonzalo Bosio <gonzaabosio@gmail.com>
2024-08-07 11:14:17 -0300

    jump and basic physics

--- go.mod
+++ go.mod
@@ -2,9 +2,11 @@ module github.com/gonzabosio/go-nowhere
 
 go 1.22.4
 
+require github.com/gen2brain/raylib-go/raylib v0.0.0-20240628125141-62016ee92fc0
+
 require (
 	github.com/ebitengine/purego v0.7.1 // indirect
-	github.com/gen2brain/raylib-go/raylib v0.0.0-20240628125141-62016ee92fc0
+	github.com/gen2brain/raylib-go/physics v0.0.0-20240628125141-62016ee92fc0
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
 	golang.org/x/sys v0.23.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,56 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"github.com/gen2brain/raylib-go/physics"
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+const screenWidth = 1280
+const screenHeight = 720
+
+const velocity = 0.5
 
 func main() {
-	rl.InitWindow(1280, 720, "Go-Nowhere")
+	rl.InitWindow(screenWidth, screenHeight, "Go-Nowhere")
 	defer rl.CloseWindow()
 
-	rl.SetTargetFPS(60)
+	physics.Init()
+	body := physics.NewBodyRectangle(rl.NewVector2(screenWidth/12, screenHeight/4*3), 40, 40, 1)
+	body.FreezeOrient = true
+
+	floor := physics.NewBodyRectangle(rl.NewVector2(screenWidth/2, screenHeight), screenWidth, 150, 1)
+	floor.Enabled = false
+
+	rl.SetTargetFPS(75)
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
-		rl.ClearBackground(rl.RayWhite)
+		rl.ClearBackground(rl.Black)
+
+		physics.Update()
+
+		body.Velocity.X = +velocity
+		if rl.IsKeyPressed(rl.KeyUp) {
+			body.Velocity.Y = -velocity * 5
+		}
+
+		// Draw physics
+		for i, body := range physics.GetBodies() {
+			vertexCount := physics.GetShapeVerticesCount(i)
+			for j := 0; j < vertexCount; j++ {
+				vertexA := body.GetShapeVertex(j)
+				jj := 0
+				if j+1 < vertexCount {
+					jj = j + 1
+				}
+				vertexB := body.GetShapeVertex(jj)
+				rl.DrawLineV(vertexA, vertexB, rl.Blue)
+			}
+		}
+
+		rl.DrawFPS(0, 0)
+
 		rl.EndDrawing()
 	}
+	physics.Close()
 }
