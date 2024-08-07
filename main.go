ffc3160ecc267996a1e86fe78138838161fe983f
Gonzalo Bosio <gonzaabosio@gmail.com>
2024-08-07 15:38:03 -0300

    camera and no jump spamming

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gen2brain/raylib-go/physics"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
@@ -9,6 +11,8 @@ const screenWidth = 1280
 const screenHeight = 720
 
 const velocity = 0.5
+
+var canJump = true
 
 func main() {
 	rl.InitWindow(screenWidth, screenHeight, "Go-Nowhere")
@@ -22,16 +26,29 @@ func main() {
 	floor.Enabled = false
 
 	rl.SetTargetFPS(75)
+	camera := rl.Camera2D{}
+	camera.Offset = rl.Vector2{X: screenWidth / 2, Y: screenHeight / 2}
+	camera.Rotation = 0.0
+	camera.Zoom = 1.0
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
 
+		rl.BeginMode2D(camera)
+		camera.Target = rl.NewVector2(body.Position.X, screenHeight/2)
+
 		physics.Update()
 
 		body.Velocity.X = +velocity
-		if rl.IsKeyPressed(rl.KeyUp) {
+		if rl.IsKeyPressed(rl.KeyUp) && canJump {
 			body.Velocity.Y = -velocity * 5
+
+			go func() {
+				canJump = false
+				time.Sleep(500 * time.Millisecond)
+				canJump = true
+			}()
 		}
 
 		// Draw physics
@@ -47,9 +64,7 @@ func main() {
 				rl.DrawLineV(vertexA, vertexB, rl.Blue)
 			}
 		}
-
-		rl.DrawFPS(0, 0)
-
+		rl.DrawFPS(int32(body.Position.X-screenWidth/2), 5)
 		rl.EndDrawing()
 	}
 	physics.Close()
